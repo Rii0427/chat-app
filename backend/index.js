ec7742c6db38ad341d473027c8ee17e5ab5893ef
Rishabh Gupta <rg074523@gmail.com>
2024-11-08 01:18:06 +0530

    set up

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//the app is listening for requests on a specific port and will log a message when it's ready
+app.listen(port, (req,res) => {
+    console.log(`ChatApp backend listening at http://localhost:${port}`);
+});
